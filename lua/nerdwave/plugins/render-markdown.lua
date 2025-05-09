3e4e6f99b804decd22a314d99e0634ac600eca03
nerdwave-nick <nick@nerdwave.io>
2025-05-10 01:24:36 +0200

    feat: render-markdown code companion rendering

--- lua/nerdwave/plugins/render-markdown.lua
+++ lua/nerdwave/plugins/render-markdown.lua
@@ -2,4 +2,6 @@ return {
   'MeanderingProgrammer/render-markdown.nvim',
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
   opts = {},
+
+  ft = { 'markdown', 'codecompanion' },
 }
